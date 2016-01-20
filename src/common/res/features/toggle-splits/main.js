d710411d85ef683e116efd0cff9faf37afcabcae
Nathanael Ritz <nathanritz@gmail.com>
2016-01-20 15:57:09 -0700

    80% of the way there. Just something strange with the toggling and stuff.

--- src/common/res/features/toggle-splits/main.js
+++ src/common/res/features/toggle-splits/main.js
@@ -1,19 +1,60 @@
-(function ynab_enhanced_toggle_splits() {
-  if (typeof Em !== 'undefined' && typeof Ember !== 'undefined' && typeof $ !== 'undefined') {
+(function poll() { 
+  // Waits until an external function gives us the all clear that we can run (at /shared/main.js)
+  if ( typeof ynabToolKit !== "undefined"  && ynabToolKit.actOnChangeInit === true ) {
+  
+    ynabToolKit.toggleSplits = new function()  { // Keep feature functions contained within this
+    	
+    	this.setting = 'init',
+    	
+ this.invoke = function() {	
 
-    if ($(".undo-redo-container").length && !$('#toggleSplits').length) {
-      var toggleButton = "<button id=\"toggleSplits\" class=\"ember-view button\"><i class=\"ember-view flaticon stroke down\"></i><i class=\"ember-view flaticon stroke right\"></i> Toggle Splits </button>"
-      $(toggleButton).insertAfter(".accounts-toolbar .undo-redo-container");
+     if ( !$('#toggleSplits').length ) {
+    	var toggleButton = "<button id=\"toggleSplits\" class=\"ember-view button\"><i class=\"ember-view flaticon stroke right\"></i><i class=\"ember-view flaticon stroke down\"></i> Toggle Splits </button>"
+    		$(toggleButton).insertAfter(".accounts-toolbar .undo-redo-container");
+     }
+     
+     // default the right arrow to hidden
+     if ( ynabToolKit.toggleSplits.setting === 'init' ) {
 
-      // default the right arrow to hidden
-      $("#toggleSplits > .right").hide();
-
-      $("#toggleSplits").bind("click", function() {
-        $(".ynab-grid-body-sub").toggle();
+    	 ynabToolKit.toggleSplits.setting = 'hide';    	 
+     }
+      
+     if ( ynabToolKit.toggleSplits.setting === 'hide' ) {
+    	 $(".ynab-grid-body-sub").hide();
+     } else {
+    	 $(".ynab-grid-body-sub").show();
+     }
+     
+      $("#toggleSplits").on("click", function() {
+    	  if ( ynabToolKit.toggleSplits.setting === 'hide' ) {
+    		 ynabToolKit.toggleSplits.setting = 'show';
+    		 $(".ynab-grid-body-sub").show();
+    	  } else {
+    	     ynabToolKit.toggleSplits.setting = 'hide';
+    	     $(".ynab-grid-body-sub").hide();
+    	  }
+    	  
         $("#toggleSplits > i").toggle();
       });
-    }
-  }
+    },
+    
+    this.observe = function(digest) {
 
-  setTimeout(ynab_enhanced_toggle_splits, 250);
-})()
+        for ( var i = 0; i < digest.length; i++ ) {
+          // We found Account transactions rows
+          if ($(digest[i]).hasClass('ynab-grid-body')) {
+            ynabToolKit.toggleSplits.invoke();
+            break;
+          }
+        }
+      }
+          
+
+    };
+    ynabToolKit.toggleSplits.invoke(); // call itself once
+
+
+    } else {
+      setTimeout(poll, 250);
+    }   
+  })();
